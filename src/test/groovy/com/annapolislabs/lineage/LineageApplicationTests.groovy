a6375ef7fce604091a6c890febfb76b6d7ba5e31
Mason Fraser <mason.fraser@ftco.ca>
2025-10-31 22:09:57 -0300

    init

---
+++ src/test/groovy/com/annapolislabs/lineage/LineageApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.annapolislabs.lineage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LineageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
